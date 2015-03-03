8a1996145fe012eb09703a79edd817db223a10d2
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2015-03-02 23:26:29 -0500

    fixed myIP and myPort

--- node.c
+++ node.c
@@ -6,6 +6,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+int getInput();
+
 int main(int argc, char ** argv)
 {
 	typedef struct {
@@ -21,19 +23,20 @@ int main(int argc, char ** argv)
 	char line[121];
 	char *item;
 	int count = -1;
-	char * myIP;
+	char myIP[20];
 	int myPort;
 
 	fp = fopen(argv[1], "r");
 
 	while (fgets(line, 120, fp)) {
 		if(count == -1) {
-			item = strtok(line, ":");
-			printf("%s ",item);
+			char * temp;
+			item = strtok_r(line, ":", &temp);
+			strcpy(myIP, item);
 
-			item = strtok(NULL, ":");
-			printf("%s ",item);
-			printf("\n");
+			item = strtok_r(NULL, ":", &temp);
+			myPort = atoi(item);
+
 			count++;
 		}
 		else {
@@ -51,33 +54,30 @@ int main(int argc, char ** argv)
 
 			item = strtok_r(NULL, " ", &temp);
 			strcpy(interfaceArr[count].remoteVIP, item);
+
 			count++;
 		}
 	}
 
 	fclose(fp);
-	exit(EXIT_SUCCESS);
 
-	// if (argc < 3) {
-	// 	printf("Command should be: myprog s <port> or myprog c <port> <address>\n");
-	// 	return 1;
-	// }
-	// int port = atoi(argv[2]);
-	// if (port < 1024 || port > 65535) {
-	// 	printf("Port number should be equal to or larger than 1024 and smaller than 65535\n");
-	// 	return 1;
-	// }
-	// if (argv[1][0] == 'c') {
-	// 	if(argv[3]==NULL){
-	// 		printf("NO IP address is given\n");
-	// 		return 1;
-	// 	}
-	// 	return client(argv[3], port);
-	// } else if (argv[1][0] == 's') {
-	// 	return server(port);
-	// } else {
-	// 	printf("unknown command type %s\nCommand should be: myprog s <port> or myprog c <port> <address>", argv[1]);
-	// 	return 1;
-	// }
-	// return 0;
+	return 0;
+
+	// return getInput();
+}
+
+int getInput() {
+	char msg[512];
+
+	while (1) {
+		fflush (stdin);
+		printf("Enter message: \n");
+		gets(msg);
+
+
+
+		printf("You type: %s\n",msg);
+	}
+
+	return 0;
 }
